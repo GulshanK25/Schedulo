c6e47137ca5a04352c7ae896f169e7035c892c75
Gulshank25 <gulshan.chhablani@gmail.com>
2025-10-22 12:03:25 +0200

    fixed doctor controller

--- Backend/controllers/admincontroller.js
+++ Backend/controllers/admincontroller.js
@@ -3,7 +3,7 @@ import doctorModel from "../models/doctormodel.js";
 import bcrypt from "bcryptjs";
 import { sendEmail } from "./emailhelper.js";
 
-// Get all users
+
 export const getAllUsersController = async (req, res) => {
   try {
     const users = await userModel.find({});
@@ -13,7 +13,7 @@ export const getAllUsersController = async (req, res) => {
   }
 };
 
-// Get all doctors
+
 export const getAllDoctorsController = async (req, res) => {
   try {
     const doctors = await doctorModel.find({});
@@ -23,20 +23,13 @@ export const getAllDoctorsController = async (req, res) => {
   }
 };
 
-// Admin adds doctor
 
 import moment from "moment";
-import { generateSlotsForDate } from "../utils/slotutils.js"; // helper to generate 30-min slots
+import { generateSlotsForDate } from "../utils/slotutils.js"; 
 
 export const adminAddDoctorController = async (req, res) => {
   try {
     const { email, password, firstName, lastName, timings } = req.body;
-
-    // timings example: { morning: { from: "09:00", to: "12:00" }, evening: { from: "14:00", to: "17:00" } }
-
-    //const existingUser = await userModel.findOne({ email });
-    //if (existingUser) 
-      //return res.status(200).send({ success: false, message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new userModel({ email, name: `${firstName} ${lastName}`, password: hashedPassword, isDoctor: true });
@@ -49,7 +42,6 @@ export const adminAddDoctorController = async (req, res) => {
       slots: [],
     });
 
-    // Generate slots for next 7 days
     const today = moment();
     for (let i = 0; i < 7; i++) {
       const date = today.clone().add(i, "days").format("DD-MM-YYYY");
@@ -66,20 +58,13 @@ export const adminAddDoctorController = async (req, res) => {
     }
 
     await newDoctor.save();
-
-    await sendEmail({
-      to: email,
-      subject: "Doctor Account Created",
-      text: `Your doctor account is ready.\nEmail: ${email}\nPassword: ${password}`,
-    });
-
     res.status(201).send({ success: true, message: "Doctor added successfully with slots" });
   } catch (error) {
     res.status(500).send({ success: false, message: error.message });
   }
 };
 
-// Change doctor application status
+
 export const changeAccountStatusController = async (req, res) => {
   try {
     const { doctorId, status } = req.body;
